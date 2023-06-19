18b5a0d74ada868029659184d9142f26152c5f20
Raju Karthikeyan <karthikeyan027@gmail.com>
2023-06-19 11:26:06 +0800

    Replase runBlocking with runTest in unit test

--- app/src/test/java/com/raju/kvr/themovie/data/repository/MoviesRepositoryTest.kt
+++ app/src/test/java/com/raju/kvr/themovie/data/repository/MoviesRepositoryTest.kt
@@ -4,12 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.raju.kvr.themovie.data.db.dao.FavouriteMovieDao
 import com.raju.kvr.themovie.data.db.entity.FavouriteMovie
-import com.raju.kvr.themovie.data.db.entity.asDomainModel
 import com.raju.kvr.themovie.data.remote.MovieApi
 import com.raju.kvr.themovie.data.remote.model.*
-import com.raju.kvr.themovie.domain.model.MovieDetail
 import com.raju.kvr.themovie.domain.model.asDbModel
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -99,111 +98,106 @@ internal class MoviesRepositoryTest {
 
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getGenres() {
-        runBlocking {
-            whenever(movieApi.getGenreList()).then { genreListResponse }
-            val genreMapResult = moviesRepository.getGenres()
+    fun getGenres() = runTest {
 
-            // verify(genreListResponse.genres.mapWithName(), times(1)) TODO
-            verify(movieApi, times(1)).getGenreList()
+        whenever(movieApi.getGenreList()).then { genreListResponse }
+        val genreMapResult = moviesRepository.getGenres()
 
-            Assert.assertEquals(5, genreMapResult.size)
-            Assert.assertEquals("Action", genreMapResult[1])
-            Assert.assertEquals("Adventure", genreMapResult[2])
-            Assert.assertEquals("Animation", genreMapResult[3])
-            Assert.assertEquals("Comedy", genreMapResult[4])
-            Assert.assertEquals("Crime", genreMapResult[5])
-        }
+        // verify(genreListResponse.genres.mapWithName(), times(1)) TODO
+        verify(movieApi, times(1)).getGenreList()
+
+        Assert.assertEquals(5, genreMapResult.size)
+        Assert.assertEquals("Action", genreMapResult[1])
+        Assert.assertEquals("Adventure", genreMapResult[2])
+        Assert.assertEquals("Animation", genreMapResult[3])
+        Assert.assertEquals("Comedy", genreMapResult[4])
+        Assert.assertEquals("Crime", genreMapResult[5])
+
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getMovies() {
-        val list: List<MovieResponse> = listOf(movieResponse, movieResponse.copy(id = 1081894))
-        val movieListResponse = MovieListResponse(1, 20, list)
-        runBlocking {
-            whenever(movieApi.getGenreList()).then { genreListResponse }
-            whenever(movieApi.getMovieList("category", 1)).then { movieListResponse }
-
-            var movies = moviesRepository.getMovies("category", 1)
-
-            verify(movieApi, times(1)).getMovieList("category", 1)
-
-            Assert.assertEquals(movies.page, movieListResponse.page)
-            Assert.assertEquals(movies.totalPages, movieListResponse.totalPages)
-            Assert.assertEquals(movies.movieList.size, movieListResponse.movies.size)
-        }
-    }
-
-    @Test
-    fun searchMovies() {
+    fun getMovies() = runTest {
         val list: List<MovieResponse> = listOf(movieResponse, movieResponse.copy(id = 1081894))
         val movieListResponse = MovieListResponse(1, 20, list)
 
-        runBlocking {
-            whenever(movieApi.getGenreList()).then { genreListResponse }
-            whenever(movieApi.searchMovies("query", 1)).then { movieListResponse }
+        whenever(movieApi.getGenreList()).then { genreListResponse }
+        whenever(movieApi.getMovieList("category", 1)).then { movieListResponse }
 
-            var moviesResult = moviesRepository.searchMovies("query", 1)
+        var movies = moviesRepository.getMovies("category", 1)
 
-            verify(movieApi, times(1)).searchMovies("query", 1)
+        verify(movieApi, times(1)).getMovieList("category", 1)
 
-            Assert.assertEquals(moviesResult.page, movieListResponse.page)
-            Assert.assertEquals(moviesResult.totalPages, movieListResponse.totalPages)
-            Assert.assertEquals(moviesResult.movieList.size, movieListResponse.movies.size)
-        }
+        Assert.assertEquals(movies.page, movieListResponse.page)
+        Assert.assertEquals(movies.totalPages, movieListResponse.totalPages)
+        Assert.assertEquals(movies.movieList.size, movieListResponse.movies.size)
     }
 
     @Test
-    fun getMovieDetail() {
-        runBlocking {
-            whenever(movieApi.getMovieDetail(1081893)).then { movieDetailResponse }
+    fun searchMovies() = runTest {
+        val list: List<MovieResponse> = listOf(movieResponse, movieResponse.copy(id = 1081894))
+        val movieListResponse = MovieListResponse(1, 20, list)
 
-            var movieDetailResult = moviesRepository.getMovieDetail(1081893)
+        whenever(movieApi.getGenreList()).then { genreListResponse }
+        whenever(movieApi.searchMovies("query", 1)).then { movieListResponse }
 
-            verify(movieApi, times(1)).getMovieDetail(1081893)
+        var moviesResult = moviesRepository.searchMovies("query", 1)
 
-            Assert.assertEquals(movieDetailResponse.id, movieDetailResult.id)
-            Assert.assertEquals(movieDetailResponse.poster, movieDetailResult.poster)
-            Assert.assertEquals(movieDetailResponse.title, movieDetailResult.title)
-            Assert.assertEquals(movieDetailResponse.genres.size, movieDetailResult.genres.size)
-            Assert.assertEquals("28/Oct/2022", movieDetailResult.releaseDate)
-            Assert.assertEquals(movieDetailResponse.voteAverage, movieDetailResult.voteAverage, 0.0)
-            Assert.assertEquals(movieDetailResponse.voteCount, movieDetailResult.voteCount, 0.0)
-            Assert.assertEquals(movieDetailResponse.backdrop, movieDetailResult.backdrop)
-            Assert.assertEquals(movieDetailResponse.tagline, movieDetailResult.tagline)
-            Assert.assertEquals(movieDetailResponse.overview, movieDetailResult.overview)
-            Assert.assertEquals(
-                movieDetailResponse.languages.size,
-                movieDetailResult.languages.size
-            )
-            Assert.assertEquals(movieDetailResponse.status, movieDetailResult.status)
-        }
+        verify(movieApi, times(1)).searchMovies("query", 1)
+
+        Assert.assertEquals(moviesResult.page, movieListResponse.page)
+        Assert.assertEquals(moviesResult.totalPages, movieListResponse.totalPages)
+        Assert.assertEquals(moviesResult.movieList.size, movieListResponse.movies.size)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun addToFavourite() {
-        runBlocking {
-            val movieDetail = movieDetailResponse.asDomainModel()
-            val favouriteMovie = movieDetail.asDbModel()
-            whenever(favouriteMovieDao.insert(favouriteMovie)).then { }
+    fun getMovieDetail() = runTest {
+        whenever(movieApi.getMovieDetail(1081893)).then { movieDetailResponse }
 
-            moviesRepository.addToFavourite(movieDetail)
+        var movieDetailResult = moviesRepository.getMovieDetail(1081893)
 
-            verify(favouriteMovieDao, times(1)).insert(favouriteMovie)
-        }
+        verify(movieApi, times(1)).getMovieDetail(1081893)
+
+        Assert.assertEquals(movieDetailResponse.id, movieDetailResult.id)
+        Assert.assertEquals(movieDetailResponse.poster, movieDetailResult.poster)
+        Assert.assertEquals(movieDetailResponse.title, movieDetailResult.title)
+        Assert.assertEquals(movieDetailResponse.genres.size, movieDetailResult.genres.size)
+        Assert.assertEquals("28/Oct/2022", movieDetailResult.releaseDate)
+        Assert.assertEquals(movieDetailResponse.voteAverage, movieDetailResult.voteAverage, 0.0)
+        Assert.assertEquals(movieDetailResponse.voteCount, movieDetailResult.voteCount, 0.0)
+        Assert.assertEquals(movieDetailResponse.backdrop, movieDetailResult.backdrop)
+        Assert.assertEquals(movieDetailResponse.tagline, movieDetailResult.tagline)
+        Assert.assertEquals(movieDetailResponse.overview, movieDetailResult.overview)
+        Assert.assertEquals(
+            movieDetailResponse.languages.size, movieDetailResult.languages.size
+        )
+        Assert.assertEquals(movieDetailResponse.status, movieDetailResult.status)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun deleteFromFavourite() {
-        runBlocking {
-            val movieDetail = movieDetailResponse.asDomainModel()
-            whenever(favouriteMovieDao.delete(movieDetail.id)).then { }
+    fun addToFavourite() = runTest {
+        val movieDetail = movieDetailResponse.asDomainModel()
+        val favouriteMovie = movieDetail.asDbModel()
+        whenever(favouriteMovieDao.insert(favouriteMovie)).then { }
 
-            moviesRepository.deleteFromFavourite(movieDetail.id)
+        moviesRepository.addToFavourite(movieDetail)
 
-            verify(favouriteMovieDao, times(1)).delete(movieDetail.id)
-        }
+        verify(favouriteMovieDao, times(1)).insert(favouriteMovie)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun deleteFromFavourite() = runTest {
+        val movieDetail = movieDetailResponse.asDomainModel()
+        whenever(favouriteMovieDao.delete(movieDetail.id)).then { }
+
+        moviesRepository.deleteFromFavourite(movieDetail.id)
+
+        verify(favouriteMovieDao, times(1)).delete(movieDetail.id)
     }
 
     @Test
@@ -219,43 +213,39 @@ internal class MoviesRepositoryTest {
         // TODO : Test Transformation by observing livedata
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getMovieFromDb() {
-        runBlocking {
-            whenever(favouriteMovieDao.getMovie(1081893)).then { favouriteMovie }
+    fun getMovieFromDb() = runTest {
+        whenever(favouriteMovieDao.getMovie(1081893)).then { favouriteMovie }
 
-            val movieDetailResult = moviesRepository.getMovieFromDb(1081893)
+        val movieDetailResult = moviesRepository.getMovieFromDb(1081893)
 
-            verify(favouriteMovieDao, times(1)).getMovie(1081893)
+        verify(favouriteMovieDao, times(1)).getMovie(1081893)
 
-            Assert.assertTrue(movieDetailResult != null)
-
-        }
+        Assert.assertTrue(movieDetailResult != null)
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getMovieFromDb_invalidMovieId_returnsNullMovieDetail() {
-        runBlocking {
-            whenever(favouriteMovieDao.getMovie(-1)).then { null }
+    fun getMovieFromDb_invalidMovieId_returnsNullMovieDetail() = runTest {
+        whenever(favouriteMovieDao.getMovie(-1)).then { null }
 
-            val movieDetailResult = moviesRepository.getMovieFromDb(-1)
+        val movieDetailResult = moviesRepository.getMovieFromDb(-1)
 
-            verify(favouriteMovieDao, times(1)).getMovie(-1)
+        verify(favouriteMovieDao, times(1)).getMovie(-1)
 
-            Assert.assertTrue(movieDetailResult == null)
-        }
+        Assert.assertTrue(movieDetailResult == null)
     }
 
     @Test
     fun getFavouriteMovies() {
-        val favouriteMovieList: LiveData<List<FavouriteMovie>> =
-            MutableLiveData(
-                listOf(
-                    favouriteMovie,
-                    favouriteMovie.copy(id = 2, movieId = 1081894),
-                    favouriteMovie.copy(id = 3, movieId = 1081895)
-                )
+        val favouriteMovieList: LiveData<List<FavouriteMovie>> = MutableLiveData(
+            listOf(
+                favouriteMovie,
+                favouriteMovie.copy(id = 2, movieId = 1081894),
+                favouriteMovie.copy(id = 3, movieId = 1081895)
             )
+        )
         whenever((favouriteMovieDao.getMovieList())).then { favouriteMovieList }
 
         moviesRepository.getFavouriteMovies()
